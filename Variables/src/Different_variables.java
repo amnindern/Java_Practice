545947d61170b96b71c9bee25ed639fc93f9d606
amnindern <amnindern@gmail.com>
2022-01-21 00:02:13 -0500

    Playing with variables

---
+++ Variables/src/Different_variables.java
@@ -0,0 +1,13 @@
+
+public class Different_variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int IntegerNumber = 10 ;
+		System.out.println(IntegerNumber);
+		
+	}
+
+}
