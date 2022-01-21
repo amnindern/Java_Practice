b334cd37ea6889026573317d925101e311ea8c3f
amnindern <amnindern@gmail.com>
2022-01-21 15:29:38 -0500

    Different variable test

--- Variables/src/Different_variables.java
+++ Variables/src/Different_variables.java
@@ -4,10 +4,16 @@ public class Different_variables {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		
-		int IntegerNumber = 10 ;
-		System.out.println(IntegerNumber);
-		
+		int myNum = 5;               // Integer (whole number)
+		float myFloatNum = 5.99f;    // Floating point number
+		char myLetter = 'D';         // Character
+		boolean myBool = true;       // Boolean
+		String myText = "Hello";     // String
+		System.out.println(myNum);
+		System.out.println(myFloatNum);
+		System.out.println(myLetter);
+		System.out.println(myBool);
+		System.out.println(myText);
 	}
 
 }
